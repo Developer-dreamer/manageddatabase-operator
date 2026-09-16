e6dd2031ee06f7ab5016b6800672ef6a4eb20fd8
Developer-dreamer <podolancukserafim@gmail.com>
2026-09-16 20:46:23 +0300

    Added manageddatabase types

---
+++ api/v1alpha1/manageddatabase_types.go
@@ -0,0 +1,75 @@
+/*
+Copyright 2026.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+)
+
+// ManagedDatabaseSpec defines the desired state of ManagedDatabase
+type ManagedDatabaseSpec struct {
+	Engine string `json:"engine"`
+	SizeGB int    `json:"sizeGB"`
+}
+
+// ManagedDatabaseStatus defines the observed state of ManagedDatabase.
+type ManagedDatabaseStatus struct {
+	State      string `json:"state,omitempty"`
+	DatabaseID string `json:"databaseID,omitempty"`
+	Endpoint   string `json:"endpoint,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+// +kubebuilder:printcolumn:name="Engine",type=string,JSONPath=`.spec.engine`
+// +kubebuilder:printcolumn:name="Size",type=integer,JSONPath=`.spec.sizeGB`
+// +kubebuilder:printcolumn:name="State",type=string,JSONPath=`.status.state`
+// +kubebuilder:printcolumn:name="Endpoint",type=string,JSONPath=`.status.endpoint`,priority=1
+
+// ManagedDatabase is the Schema for the manageddatabases API
+type ManagedDatabase struct {
+	metav1.TypeMeta `json:",inline"`
+
+	// metadata is a standard object metadata
+	// +optional
+	metav1.ObjectMeta `json:"metadata,omitzero"`
+
+	// spec defines the desired state of ManagedDatabase
+	// +required
+	Spec ManagedDatabaseSpec `json:"spec"`
+
+	// status defines the observed state of ManagedDatabase
+	// +optional
+	Status ManagedDatabaseStatus `json:"status,omitzero"`
+}
+
+// +kubebuilder:object:root=true
+
+// ManagedDatabaseList contains a list of ManagedDatabase
+type ManagedDatabaseList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitzero"`
+	Items           []ManagedDatabase `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(func(s *runtime.Scheme) error {
+		s.AddKnownTypes(SchemeGroupVersion, &ManagedDatabase{}, &ManagedDatabaseList{})
+		return nil
+	})
+}
